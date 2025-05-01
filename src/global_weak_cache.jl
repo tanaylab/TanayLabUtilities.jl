754cfef67a6f5220f1738da2b9c92939eb9841e7
Oren Ben-Kiki <oren@ben-kiki.org>
2025-05-01 17:30:41 +0300

    Make the global cache depend on path mtime.

--- src/global_weak_cache.jl
+++ src/global_weak_cache.jl
@@ -9,41 +9,47 @@ using Base.Threads
 
 GLOBAL_LOCK = ReentrantLock()
 
-GLOBAL_CACHE = Dict{Any, WeakRef}()
+GLOBAL_CACHE = Dict{Tuple{AbstractString, Any}, Tuple{Float64, WeakRef}}()
 
 """
-    get_through_global_weak_cache(getter::Function, key::Any)::Any
+    get_through_global_weak_cache(getter::Function, path::AbstractString, key::Any)::Any
 
-Given some unique `key`, fetch the associated object from the global cache (if it is there) or invoke the `getter` to
-obtain it (and cache it for future reference). As a convenience, the `getter` is passed the `key`.
-If `purge` is set, then any existing value is ignored and is replaced by the result of the `getter`.
+Get some object associated with a `path` and identified by the `key from the global cache (if it is there) or invoke the
+`getter` to obtain it (and cache it for future reference). As a convenience, the `getter` is passed the `path` and
+`key`. If `purge` is set, then any existing value is ignored and is replaced by the result of the `getter`. If the
+`mtime` of the file does not match the value when the object was cached, it is likewise ignored and `getter` is called
+to fetch a fresh value.
 
 There's a singleton `GLOBAL_CACHE` and all accesses are protected by a single `GLOBAL_LOCK`, so while this is
-thread-safe, it does not support parallelism. It is meant for heavy operations such as opening files etc., not for
+thread-safe, it does not support parallelism. It is meant for "expensive" OS operations such as `mmap`, not for
 memoizing functions results for performance.
 
 The cache holds weak references to the objects, so they are let go if there remain no active references to them (outside
 the cache).
 
+!!! note
+
+    Use absolute paths to ensure correct results.
+
 ```jldoctest
 using Test
 
-first = get_through_global_weak_cache(:test) do _
+first = get_through_global_weak_cache("fake path", :test) do _
     return [1]
 end
 @test first == [1]
-@test first === get_through_global_weak_cache(:test) do _
+@test first === get_through_global_weak_cache("fake path", :test) do _
     @assert false
 end
 
-second = get_through_global_weak_cache(:test; purge = true) do _
+second = get_through_global_weak_cache("fake path", :test; purge = true) do _
     return [2]
 end
 @assert second == [2]
 
 first = second = nothing
 gc()
-third = get_through_global_weak_cache(:test) do _
+third = get_through_global_weak_cache("fake path", :test) do _
     return [3]
 end
 @assert third == [3]
@@ -54,21 +60,26 @@ println("OK")
 
 OK
 """
-function get_through_global_weak_cache(getter::Function, key::Any; purge::Bool = false)::Any
+function get_through_global_weak_cache(getter::Function, path::AbstractString, key::Any; purge::Bool = false)::Any
     return lock(GLOBAL_LOCK) do
-        if purge
-            weak_ref = nothing
-        else
-            weak_ref = get(GLOBAL_CACHE, key, nothing)
+        current_mtime = stat(path).mtime
+        cached_mtime = nothing
+        cached_weak_ref = nothing
+
+        if !purge
+            cached = get(GLOBAL_CACHE, (path, key), nothing)
+            if cached !== nothing
+                cached_mtime, cached_weak_ref = cached
+            end
         end
 
-        if weak_ref === nothing || weak_ref.value === nothing
-            @debug "Get $(key)"
+        if cached_weak_ref === nothing || cached_weak_ref.value === nothing || current_mtime != cached_mtime
+            @debug "Get $(key) for $(path)"
             value = getter(key)
-            GLOBAL_CACHE[key] = WeakRef(value)
+            GLOBAL_CACHE[(path, key)] = (current_mtime, WeakRef(value))
         else
-            @debug "Reuse $(key)"
-            value = weak_ref.value
+            @debug "Reuse $(key) for $(path)"
+            value = cached_weak_ref.value
         end
 
         return value
