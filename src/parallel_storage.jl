045a65e3b1433dd745fa1757660349e7a7f47a1b
Oren Ben-Kiki <oren@ben-kiki.org>
2025-05-28 09:04:01 +0300

    Use overloading for reset of parallel storage.

--- src/parallel_storage.jl
+++ src/parallel_storage.jl
@@ -11,6 +11,7 @@ module ParallelStorage
 export ReusableStorage
 export get_reusable!
 export put_reusable!
+export reset_reusable_storage!
 export with_reusable
 
 using Base.Threads
@@ -21,25 +22,27 @@ using ..Types
 
 Implement resuable storage for parallel computations. This allocates and reuses the minimal number of instances of some
 data for (re)use by parallel tasks. The `create` function should return a new instance of the data. If a previous task
-has used the data, we will `reset` it to bring it back it to its initial state.
+has used the data, we will call [`reset_reusable_storage!`]@(ref) to bring it back it to its initial state.
 
 !!! note
 
-    The `create` and `reset` functions must be thread safe. We intentionally do not perform them while holding the
-    global lock to maximize performance.
+    The `create` and `reset_reusable_storage!` functions must be thread safe. We intentionally do not perform them while
+    holding the global lock to maximize performance. This only matters if these functions access global variables or the
+    like; typically they just allocate some memory and work on the specific reusable storage instance, which only they
+    have access to, so this isn't a concern.
 
 ```jldoctest
 mutable struct ExampleStorage
     is_clean::Bool
 end
 
-function reset(storage::ExampleStorage)::Nothing
+function TanayLabUtilities.reset_reusable_storage!(storage::ExampleStorage)::Nothing
     @assert !storage.is_clean
     storage.is_clean = true
     return nothing
 end
 
-reusable_storage = ReusableStorage(reset) do
+reusable_storage = ReusableStorage() do
     return ExampleStorage(true)
 end
 
@@ -78,20 +81,19 @@ mutable struct ReusableStorage{T}
     lock::SpinLock
     data::Vector{T}
     create::Function
-    reset::Maybe{Function}
     is_reset::Bool
 end
 
-function ReusableStorage(create::Function, reset::Maybe{Function} = nothing)::ReusableStorage
-    return ReusableStorage(SpinLock(), [create()], create, reset, true)
+function ReusableStorage(create::Function)::ReusableStorage
+    return ReusableStorage(SpinLock(), [create()], create, true)
 end
 
 """
     get_reusable!(reusable_storage::ReusableStorage{T})::T where{T}
 
 Get a private instance of the data from the reusable storage. Will prefer to return an existing instance of the data,
-after being `reset` if used by a previous task. If all instances are currently being used by other tasks, will `create`
-a new instance instead.
+after calling [`reset_reusable_storage!`](@ref) if used by a previous task. If all instances are currently being used by
+other tasks, will `create` a new instance instead.
 """
 function get_reusable!(reusable_storage::ReusableStorage{T})::T where {T}
     data, create, reset = lock(reusable_storage.lock) do
@@ -113,8 +115,8 @@ function get_reusable!(reusable_storage::ReusableStorage{T})::T where {T}
         data = reusable_storage.create()
     end
 
-    if reset && reusable_storage.reset !== nothing
-        reusable_storage.reset(data)
+    if reset
+        reset_reusable_storage!(data)
     end
 
     return data
@@ -124,7 +126,7 @@ end
     put_reusable!(reusable_storage::ReusableStorage{T}, data::T)::Nothing
 
 Put back data into the `reusable_storage` once a task is done with it. This will allow other tasks to reuse this data
-(after being `reset`).
+(after going through [`reset_reusable_storage!`](@ref)).
 """
 function put_reusable!(reusable_storage::ReusableStorage{T}, data::T)::Nothing where {T}
     lock(reusable_storage.lock) do
@@ -147,6 +149,15 @@ function with_reusable(action::Function, reusable_storage::ReusableStorage)::Any
     finally
         put_reusable!(reusable_storage, data)
     end
+end
+
+"""
+    function reset_reusable_storage!(reusable_storage::Any)::Nothing
+
+Given `reusable_storage` that was used by a task, return it to its initial state so it can be used by another task.
+"""
+function reset_reusable_storage!(::Any)::Nothing  # UNTESTED
+    return nothing
 end
 
 end
--- src/parallel_storage.md
+++ src/parallel_storage.md
@@ -3,9 +3,10 @@
 ```@docs
 TanayLabUtilities.ParallelStorage
 TanayLabUtilities.ParallelStorage.ReusableStorage
+TanayLabUtilities.ParallelStorage.with_reusable
 TanayLabUtilities.ParallelStorage.get_reusable!
 TanayLabUtilities.ParallelStorage.put_reusable!
-TanayLabUtilities.ParallelStorage.with_reusable
+TanayLabUtilities.ParallelStorage.reset_reusable_storage!
 ```
 
 ## Index
