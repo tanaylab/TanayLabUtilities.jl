452b8add99d66e686c17f68549d139c5a4b365f5
orenbenkiki <oren@ben-kiki.org>
2026-06-07 10:41:31 +0300

    More robust static analysis scripts.

--- deps/jet.sh
+++ deps/jet.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -e -o pipefail
+julia --project=. -e 'using Pkg; Pkg.resolve(); Pkg.instantiate(); Pkg.precompile()'
 julia --project=deps/jet_env -e 'using Pkg; Pkg.resolve(); Pkg.instantiate()'
 JULIA_DEBUG="" julia --project=deps/jet_env --color=no deps/jet.jl 2>&1 | python3 deps/jet.py
--- deps/static_analysis.sh
+++ deps/static_analysis.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -e -o pipefail
+julia --project=. -e 'using Pkg; Pkg.resolve(); Pkg.instantiate(); Pkg.precompile()'
 julia --project=deps/lint_env -e 'using Pkg; Pkg.resolve(); Pkg.instantiate()'
 julia --project=deps/lint_env deps/static_analysis.jl
